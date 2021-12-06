093358de77aa5026bfc78d2e6b7fcff722ffef76
Jahaan Jain <jahaanjain@gmail.com>
2021-12-06 10:46:16 -0500

    fixed spacing in signin page

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,31 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const Main = () => {
-  return (
-    <Container>
-      <h1>
-        Your Education Can Make a Difference!
-      </h1>
-    </Container>
-  );
+    return (
+        <Container>
+            <h1>Your Education Can Make a Difference!</h1>
+        </Container>
+    );
 };
 
 const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  h1 {
-    font-size: 65px;
-    font-weight: 70000;
-    color: #fff;
+    h1 {
+        font-size: 65px;
+        font-weight: 70000;
+        color: #fff;
+        padding-left: 3vw;
 
-    @media (max-width: 700px) {
-      display: none;
+        @media (max-width: 700px) {
+            display: none;
+        }
     }
-  }
 `;
 
 export default Main;
